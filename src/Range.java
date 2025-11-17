6ff3d1a13fa90b91e95c743450ebc3fd9c9dd906
PortugueseWolf <danieljdferreira@hotmail.com>
2025-11-17 16:40:33 +0000

    Exercise 4 completed

--- src/Range.java
+++ src/Range.java
@@ -1,20 +1,22 @@
 
 void main() {
-    Range g = new Range(5,10);
+    Range g = new Range(5, 10);
     print(g);
     printReverse(g);
     int[] numbers = naturals(new Range(1, 5));
     IO.println(Arrays.toString(numbers));
+    print(findFirst("brown", "The quick brown fox jumps over the lazy dog"));
 }
 
-record Range(int min, int max){
+
+record Range(int min, int max) {
 
     boolean isEmpty() {
         return min > max;
     }
 
     int length() {
-        if (isEmpty()){
+        if (isEmpty()) {
             return 0;
         }
         return max - min + 1;
@@ -23,7 +25,7 @@ record Range(int min, int max){
 
 void print(Range range) {
     for (int i = range.min; i <= range.max; i++) {
-        if (i != range.max){
+        if (i != range.max) {
             IO.print(i + " ");
         } else {
             IO.println(i);
@@ -32,8 +34,8 @@ void print(Range range) {
 }
 
 void printReverse(Range range) {
-    for (int i = range.max; i >= range.min ; i--) {
-        if (i != range.min){
+    for (int i = range.max; i >= range.min; i--) {
+        if (i != range.min) {
             IO.print(i + " ");
         } else {
             IO.println(i);
@@ -41,13 +43,23 @@ void printReverse(Range range) {
     }
 }
 
+
 int[] naturals(Range range) {
     int[] array = new int[range.length()];
     int value = range.min;
 
-    for (int i = 0; i < array.length ; i++) {
+    for (int i = 0; i < array.length; i++) {
         array[i] = value;
-        value ++;
+        value++;
     }
     return array;
 }
+
+Range findFirst(String search, String text) {
+    int index = text.indexOf(search);
+    if (index == -1){
+        return null;
+    }
+    return new Range(index, index + search.length() - 1);
+}
+
